a980eefd5e60ac641a5bd63a6f941e9207b8a756
zakimal <oza777you@gmail.com>
2018-10-27 09:55:56 +0900

    implemented StmtExp

--- interp.ml
+++ interp.ml
@@ -3,13 +3,13 @@ type id = string
 type stmt = Stmts of stmt * stmt
           | Assign of id * exp
           | Print of exp
-
 and exp = Id of id
         | Num of int
         | Plus of exp * exp
         | Minus of exp * exp
         | Times of exp * exp
         | Div of exp * exp
+        | StmtExp of stmt * exp
 
 exception No_such_symbol
 
@@ -21,7 +21,6 @@ let rec trans_stmt ast env =
           Stmts (s1, s2) -> let env' = trans_stmt s1 env in trans_stmt s2 env'
         | Assign (var, e) -> let vl = trans_exp e env in update var vl env
         | Print e -> let vl = trans_exp e env in (print_int vl; print_string "\n"; env)
-
 and trans_exp ast env =
     match ast with
           Id v -> env v
@@ -38,6 +37,7 @@ and trans_exp ast env =
         | Div (e1, e2) -> let vl1 = trans_exp e1 env in
                                 let vl2 = trans_exp e2 env in
                                     vl1 / vl2
+        | StmtExp (s, e) -> let env' = trans_stmt s env in trans_exp e env'
 
 let prog = Stmts (Assign ("x", Plus (Num 1, Times (Num 2, Num 3))),
             Stmts (Assign ("y", Div (Id "x", Num 4)), Print (Id "y")))
